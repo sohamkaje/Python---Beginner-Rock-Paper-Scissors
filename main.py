d3a421fb9b51736c173e70532c3ba7da22853404
sohamkaje <119487401+sohamkaje@users.noreply.github.com>
2023-01-02 19:07:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 import random
 
-global choice
-global generatednum
-
+# This function generates the AI's choice, asks the player for their choice,
+# makes sure that the player's choice is a valid option, announces the AI's choice, then calls the decide function 
 def rungame():
-  global choice
-  global generatednum
-  generatednum = random.randint(1,3)
-  if generatednum == 1:
+  generatednum = random.randint(1,3) # randomly generates a number between 1 and 3
+  if generatednum == 1: # if-else chain to change numbers to options 
     generatednum = "rock"
   elif generatednum == 2:
     generatednum = "paper"
   elif generatednum == 3:
     generatednum = "scissors"
-  choice = str(input("Rock, Paper, or Scissors? "))
+  choice = str(input("Rock, Paper, or Scissors?  # asks player for their choice
   choiceslist = ["rock", "paper", "scissors"]
-  while choice.lower() not in choiceslist:
+  while choice.lower() not in choiceslist: # makes sure the player's choice is a valid option
     print("That's not an option!")
     choice = str(input("Rock, Paper, or Scissors? "))
-  print("Your opponent chose "+ generatednum +".")
-  decide(generatednum, choice)
-  
+  print("Your opponent chose "+ generatednum +".") # lets the player know what their opponent chose
+  decide(generatednum, choice) # calls the decide function to determine the winner
+
+# This function compares the player's choice with the AI's choice and decides the winner
 def decide(generatednum, choice):
   choice = choice.lower()
-  if generatednum == "rock":
+  if generatednum == "rock": # nested if-else chain to conclude the winner
     if choice == "rock":
       print("Tie!")
     elif choice == "paper":
@@ -46,7 +44,7 @@ def decide(generatednum, choice):
       print("Tie!")
 
 runGame = True
-while runGame:
+while runGame: # while loop to decide whether to re-run the game
   rungame()
   repeat = str(input("Would you like to play again? Yes or No! "))
   if repeat.lower() == "yes":
